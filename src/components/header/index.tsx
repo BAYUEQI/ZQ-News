7be46bd0539c8d727c481adb0ee35206b9e0e381
BAYUEQI <zouqi050813@gmail.com>
2025-06-27 22:28:48 +0800

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -46,11 +46,8 @@ export function Header() {
         <Link to="/" className="flex gap-2 items-center">
           <div className="h-10 w-10 bg-cover" title="logo" style={{ backgroundImage: "url(/icon.svg)" }} />
           <span className="text-2xl font-brand line-height-none!">
-            <p>News</p>
-            <p className="mt--1">
-              <span className="color-primary-6">N</span>
-              <span>ow</span>
-            </p>
+            <p>ZQ</p>
+            <p className="mt--1">News</p>
           </span>
         </Link>
       </span>
